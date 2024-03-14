f398a99409e568136a77ed9a41ee9815215fb994
federico-casu <federico.casu1101@gmail.com>
2024-03-14 16:39:21 +0100

    feat: added all movie info in each card

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -21,12 +21,17 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <h1>Movies</h1>
+        <div class="row d-flex">
             @foreach ($movies as $movie)
                 <div class="card col-4">
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column gap-2">
+                        <span>Id: {{ $movie->id }}</span>
                         <h4 class="card-title">{{ $movie->title }}</h4>
-                        <p class="card-text">Text</p>
+                        <h6 class="card-title">{{ $movie->original_title }}</h6>
+                        <span>Nationality: {{ ucfirst($movie->nationality) }}</span>
+                        <span>Date: {{ $movie->date }}</span>
+                        <span>Vote: {{ $movie->vote }}</span>
                     </div>
                 </div>
             @endforeach
