dc9e95971b1717b06a09fe617f8ebce32231558f
federico-casu <federico.casu1101@gmail.com>
2024-03-14 20:03:15 +0100

    feat: added gap between cards

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -22,9 +22,9 @@
 @section('content')
     <div class="container">
         <h1>Movies</h1>
-        <div class="row d-flex">
+        <div class="row justify-content-between">
             @foreach ($movies as $movie)
-                <div class="card col-4">
+                <div class="card">
                     <div class="card-body d-flex flex-column gap-2">
                         <span>Id: {{ $movie->id }}</span>
                         <h4 class="card-title">{{ $movie->title }}</h4>
