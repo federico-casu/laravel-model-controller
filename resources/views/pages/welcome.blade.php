7c315a9feca4c8d4b20987fd94b27f1f5db1c892
federico-casu <federico.casu1101@gmail.com>
2024-03-15 10:20:00 +0100

    fix: fix to syntax to access information from db data collection

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -26,12 +26,12 @@
             @foreach ($movies as $movie)
                 <div class="card">
                     <div class="card-body d-flex flex-column gap-2">
-                        <span>Id: {{ $movie->id }}</span>
-                        <h4 class="card-title">{{ $movie->title }}</h4>
-                        <h6 class="card-title">{{ $movie->original_title }}</h6>
-                        <span>Nationality: {{ ucfirst($movie->nationality) }}</span>
-                        <span>Date: {{ $movie->date }}</span>
-                        <span>Vote: {{ $movie->vote }}</span>
+                        <span>Id: {{ $movie['id'] }}</span>
+                        <h4 class="card-title">{{ $movie['title'] }}</h4>
+                        <h6 class="card-title">{{ $movie['original_title'] }}</h6>
+                        <span>Nationality: {{ ucfirst($movie['nationality']) }}</span>
+                        <span>Date: {{ $movie['date'] }}</span>
+                        <span>Vote: {{ $movie['vote'] }}</span>
                     </div>
                 </div>
             @endforeach
